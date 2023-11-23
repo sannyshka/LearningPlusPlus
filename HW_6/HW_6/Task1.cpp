5b864897608f5bafe42c6a377403731d20c3255e
Aleksandr Astapenkov <s.alexandr.valerievich@g.gmail.com>
2023-11-23 14:06:48 +0200

    HW #6 completed

---
+++ HW_6/HW_6/Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+    double firstElement, step, lastElement;
+    std::cout << "Enter the first element: ";
+    std::cin >> firstElement;
+    std::cout << "Enter the arithmetic progression step: ";
+    std::cin >> step;
+    std::cout << "Enter the number of the last item: ";
+    std::cin >> lastElement;
+
+    for (double currentElement = firstElement; currentElement <= lastElement;
+        currentElement += step) {
+        std::cout << currentElement << " ";
+    }
+
+    return 0;
+}
+
---
+++ HW_6/HW_6/Task5.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cctype>
+
+int main()
+{
+    char inputChar;
+    int digitSum = 0;
+
+    while (true) {
+        std::cout << "Enter a character (enter '.' to complete): ";
+        std::cin >> inputChar;
+
+        if (inputChar == '.') {
+            std::cout << "The program is complete." << std::endl;
+            break;
+        }
+        else if (islower(inputChar)) {
+            char upperCaseChar = toupper(inputChar);
+            std::cout << "The given letter: " << upperCaseChar << std::endl;
+        }
+        else if (isdigit(inputChar)) {
+            int digitValue = inputChar - '0';
+            digitSum += digitValue;
+            std::cout << "Current sum of digits: " << digitSum << std::endl;
+        }
+        else {
+            std::cout << "The character is not processed by the program." << std::endl;
+        }
+
+    }
+
+    return 0;
+}
