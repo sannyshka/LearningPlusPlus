090956805186ff9bb5c1ca3cc1930c1de980c8ea
Aleksandr Astapenkov <s.alexandr.valerievich1997@gmail.com>
2023-12-05 17:49:57 +0200

    HW 9 is done

---
+++ HW_9/HW_9/Task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void toUppercase(char str[]) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+}
+
+int main() {
+    const int maxSize = 100;
+    char str[maxSize];
+
+    std::cout << "Enter a string: ";
+    std::cin.getline(str, maxSize);
+
+    toUppercase(str);
+
+    std::cout << "Uppercase string: " << str;
+
+    return 0;
+
+}
